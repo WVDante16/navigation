e49857d957d01c6cfa960a0927430e6c7e6780bd
unknown <walbertozekrom@gmail.com>
2022-05-02 12:01:03 -0500

    Navigation

---
+++ HomeScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import './styles';
+import styles from './styles';
+
+function HomeScreen() {
+    return (
+        <View style = {styles.container}>
+            <Text>Home</Text>
+        </View>
+    );
+}
+
+export default HomeScreen;
